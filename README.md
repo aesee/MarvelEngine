691c6158cfa67c1a6394cf2e0a4825559ff78b14
aesee <aesee@mail.ru>
2018-10-30 23:44:37 +0600

    Add glew init

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 # MarvelEngine
 The game engine
 
-## Using libraries
-
-There are some third party libraries do you need to build this solution.
-* DXUT. Place dxut folder on source/3rdParty. Add references on to Core and Optional projects if they are removed.
-
-Besides you need to install Directx SDK that currently deprecated by Microsoft.
+The project start again from scratch. Now it uses an OpenGL instead DirectX.
--- Source/Engine/main.cpp
+++ Source/Engine/main.cpp
@@ -4,13 +4,43 @@
 // GLFW
 #include "..\3rdParty\GLFW\glfw3.h"
 
+#include <iostream>
+
 int main()
 {
+	// Init GLFW
 	glfwInit();
+	// Set a require versions of opengl
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Set profile
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Not allow to resize window
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create a window
+	int width = 800;
+	int height = 600;
+	GLFWwindow* window = glfwCreateWindow(width, height, "Marvel Engine", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create a window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Init GLEW
+	glewExperimental = GL_TRUE;	// Using an experimental features
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	// Set viewport
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
 
 	return 0;
 }
